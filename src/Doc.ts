425e324b26c3b45724ac3d880f449166950b26a3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-10-19 09:48:18 +0900

    Implemented find in Doc

--- src/Doc.ts
+++ src/Doc.ts
@@ -1,7 +1,7 @@
 import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 
-import { ParagraphItem, Item, ItemType, ItemData } from './Item';
+import { ParagraphItem, Item, ItemType, ItemDataContent } from './Item';
 
 const itemMap = {
   paragraph: ParagraphItem,
@@ -41,21 +41,40 @@ export class Doc {
   }
 
   /* Traverse */
+  public itemsData() {
+    const data = this.data.getMap('data');
+    const itemsData: Y.Array<Y.Map<ItemDataContent>> = data.get('items');
+    return itemsData;
+  }
+
   public _first(): Item | null {
     const data = this.data.getMap('data');
-    const items: Y.Array<Y.Map<ItemData>> = data.get('items');
-    const item: Y.Map<ItemData> | null = items.get(0) || null;
-    if (item) {
-      const tmp: ItemData = item.toJSON();
-      const ItemClass = itemMap[tmp.type];
-      const itm = new ItemClass(item);
+    const itemsData: Y.Array<Y.Map<ItemDataContent>> = data.get('items');
+    const itemData: Y.Map<ItemDataContent> | null = itemsData.get(0) || null;
+    if (itemData) {
+      const itemDataContent = itemData.toJSON();
+      const ItemClass = itemMap[itemDataContent.type];
+      const itm = new ItemClass(itemData);
       itm.doc = this;
       return itm;
     }
     return null;
   }
 
-  public find(): Item | null {
+  public find(itemId: string): Item | null {
+    const data = this.data.getMap('data');
+    const itemsData: Y.Array<Y.Map<ItemDataContent>> = data.get('items');
+    for (let i = 0; i < itemsData.length; i += 1) {
+      const itemData: Y.Map<ItemDataContent> = itemsData.get(i);
+      const itemDataContent: ItemDataContent = itemData.toJSON();
+      if (itemDataContent.id === itemId) {
+        const itemDataContent = itemData.toJSON();
+        const ItemClass = itemMap[itemDataContent.type];
+        const itm = new ItemClass(itemData);
+        itm.doc = this;
+        return itm;
+      }
+    }
     return null;
   }
 
@@ -63,7 +82,7 @@ export class Doc {
   public append(item: Item): void {
     const data = this.data.getMap('data');
     const items: Y.Array<any> = data.get('items');
-    items.push([item.data]);
+    items.push([item.getData()]);
   }
 
   /* Methods */
