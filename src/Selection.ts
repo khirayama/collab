bc44837106bff0d4059d00359278e8c1050ae87a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-10-19 09:48:04 +0900

    Update Selection

--- src/Selection.ts
+++ src/Selection.ts
@@ -1,3 +1,5 @@
+import * as Y from 'yjs';
+
 import { Doc } from './Doc';
 
 export class Selection {
@@ -8,11 +10,11 @@ export class Selection {
     range: {
       anchor: {
         id: string;
-        offset: number;
+        offset: Y.RelativePosition;
       };
       focus: {
         id: string;
-        offset: number;
+        offset: Y.RelativePosition;
       };
     } | null;
   } = {
@@ -25,12 +27,40 @@ export class Selection {
   }
 
   public toJSON() {
-    return this.data;
+    return {
+      compositionText: this.data.compositionText,
+      range:
+        this.data.range === null
+          ? null
+          : {
+              anchor: {
+                id: this.data.range.anchor.id,
+                offset: this.data.range.anchor.offset /* index */,
+              },
+              focus: {
+                id: this.data.range.focus.id,
+                offset: this.data.range.focus.offset /* index */,
+              },
+            },
+    };
   }
 
   /* tmp */
   public _focus() {
     const item = this.doc._first();
-    console.log(item);
+    const text: Y.Text = item.get('text') as any;
+    if (item && text) {
+      const itemDataContent = item.toJSON();
+      this.data.range = {
+        anchor: {
+          id: itemDataContent.id,
+          offset: Y.createRelativePositionFromTypeIndex(text, 1),
+        },
+        focus: {
+          id: itemDataContent.id,
+          offset: Y.createRelativePositionFromTypeIndex(text, 1),
+        },
+      };
+    }
   }
 }
