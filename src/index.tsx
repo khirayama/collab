06d4f97c8a18bd9da224a82eb6563a489c5eb6b6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-10-28 07:12:32 +0900

    Change the flow

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Doc } from './Doc';
-import { Selection } from './Selection';
-import { DebugHelper } from './components/DebugHelper';
+import { Document } from './Document';
+import { User } from './User';
+import { Editor } from './components/Editor';
 
-type EditorProps = {
-  doc: Doc;
-  selection: Selection;
-};
-
-type EditorState = {
-  doc: ReturnType<Doc['toJSON']>;
-  selection: ReturnType<Selection['toJSON']>;
-};
-
-class Editor extends React.Component<EditorProps, EditorState> {
-  constructor(props: EditorProps) {
-    super(props);
-
-    this.state = {
-      doc: this.props.doc.toJSON(),
-      selection: this.props.selection.toJSON(),
-    };
-  }
-
-  public componentDidMount(): void {
-    doc.on('update', () => {
-      this.setState({
-        doc: this.props.doc.toJSON(),
-        selection: this.props.selection.toJSON(),
-      });
-    });
-  }
-
-  public render(): JSX.Element {
-    return (
-      <>
-        <DebugHelper doc={this.props.doc} selection={this.props.selection} />
-      </>
-    );
-  }
-}
-
-const doc = new Doc();
-const selection = new Selection(doc);
-selection._focus();
+const user = new User();
+const doc = new Document(user);
 
 window.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Editor doc={doc} selection={selection} />, window.document.querySelector('#root'));
+  ReactDOM.render(<Editor doc={doc} />, window.document.querySelector('#root'));
 });
